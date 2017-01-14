a26c11187a1e96e18f584a4ce00ad9b9163cb44a
tonyj <tonyj@Brother-Entity.FT-THOMAS.ketsds.net>
2017-01-14 10:36:34 -0500

    test2

--- src/org/usfirst/frc/team554/robot/OI.java
+++ src/org/usfirst/frc/team554/robot/OI.java
@@ -10,7 +10,7 @@ import org.usfirst.frc.team554.robot.commands.ExampleCommand;
  */
 public class OI {
 	
-	//This is a test of the GIT system
+	//This is test of the GIT system
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
